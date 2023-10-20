91ca64c59af83dd89a9438a4cc1d6fef70746317
irshathbanu-mediwave <irshathbanu@mindwaveventures.com>
2023-10-20 10:45:34 +0530

    ADD PROGESS

--- app/src/components/Progess.jsx
+++ app/src/components/Progess.jsx
@@ -1,16 +1,41 @@
-import { useState, useRef } from "react";
-
-function Progess({ tasks, handledelete }) {
-  const onDragStart = (event, id) => {
-    console.log("dragstart", id);
+function progess({ tasks, handledelete }) {
+  const ondragstart = (event, id) => {
+    console.log("id-progess", id);
     event.dataTransfer.setData("id", id);
   };
   return (
     <>
-      <div className="show-card">
-        {tasks.filter((t) => t.inState === "progess")}
+      <div className="showCards">
+        {tasks
+          .filter((t) => t.inState === "progess")
+          .map((task, index) => (
+            <div
+              key={task.id}
+              className="card"
+              draggable
+              onDragStart={(e) => {
+                ondragstart(e, task.id);
+              }}
+            >
+              <div className="title-bar">
+                Task:
+                <button
+                  className="btn btn-danger Deletebtn"
+                  onClick={() => {
+                    handledelete(task.id);
+                  }}
+                >
+                  x
+                </button>
+              </div>
+
+              <div className="textarea" key={task.id}>
+                {task.text}
+              </div>
+            </div>
+          ))}
       </div>
     </>
   );
 }
-export default Progess;
+export default progess;
